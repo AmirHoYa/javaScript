e06335ac984e1b1e437ca2e04f4e8880ce8e13f0
Miya <miya@wsm.de>
2024-09-14 14:26:16 +0200

    test

---
+++ frontend/pages/equipment/script.js
@@ -0,0 +1,22 @@
+const header = document.getElementsByTagName("header")[0];
+var isHeaderSmall = false;
+
+window.onscroll = function() {resizeHeaderCheck()};
+
+function resizeHeaderCheck() {
+  if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) && !isHeaderSmall) {
+    smallHeader();
+    isHeaderSmall = true;
+  } else if (!(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) && isHeaderSmall) {
+    bigHeader();
+    isHeaderSmall = false;
+  }
+} 
+
+function bigHeader() {
+    
+}
+
+function smallHeader() {
+
+}
